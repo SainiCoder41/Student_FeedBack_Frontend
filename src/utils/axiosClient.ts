b7d9175cbbd0de1782ac77a258e1b427b6b4ab8c
SainiCoder41 <sainicoder41@gmail.com>
2025-10-02 19:41:57 +0530

    Second Commit

--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://student-feedback-backend-one.vercel.app/',
    withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
